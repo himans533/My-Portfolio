156228f2e8ca8c8e04dcf87a0ed44bd9915664c8
Himanshu <154817641+himans533@users.noreply.github.com>
2026-04-21 15:51:30 +0530

    Update social links for LinkedIn and GitHub

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -12,7 +12,7 @@ const SocialLinks = () => {
           LinkedIn <FaLinkedin size={30} />
         </>
       ),
-      href: "https://linkedin.com",
+      href: "https://www.linkedin.com/in/himanshu-hanwat/",
       style: "rounded-tr-md",
     },
     {
@@ -22,7 +22,7 @@ const SocialLinks = () => {
           GitHub <FaGithub size={30} />
         </>
       ),
-      href: "https://github.com/theyashpatel",
+      href: "https://github.com/himans533/",
     },
     {
       id: 3,
